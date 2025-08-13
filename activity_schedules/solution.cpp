8d5111a930a1ec421a7b8370acbc2c5280ac4827
Nora Al-Ashqar <noraashqar244@gmail.com>
2025-08-14 01:05:32 +0300

    solution.cpp

--- activity_schedules/solution.cpp
+++ activity_schedules/solution.cpp
@@ -2,27 +2,20 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 vector<string> activities = { "Swimming", "Running", "Football" };
 vector<string> schedule;
 int  c = 0;
-
 void schdl(int day, int n) {
-   
     if (day == n) {
         for (int i = 0; i < n; i++) {
             cout << schedule[i];
-            if (i != n - 1) cout << " ";
-            
+            if (i != n - 1) cout << " ";  
         }
         cout << endl;
         c++;
         return;
     }
-
-   
     for (string activity : activities) {
-        
         if (day == 0 || activity != schedule[day - 1]) {
             schedule.push_back(activity);         
             schdl(day + 1, n);   
@@ -30,7 +23,6 @@ void schdl(int day, int n) {
         }
     }
 }
-
 int main() {
     int n;
     cin >> n;
